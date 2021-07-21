9c836885196f16c4b81cb94ed2284ae0bf830a01
Yu MIAO <yuuu.miao@gmail.com>
2021-07-21 04:22:36 +0200

    production

--- server/app.js
+++ server/app.js
@@ -9,24 +9,25 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const mongoose = require("mongoose");
 const app = express();
-const cors = require("cors")
-
+const cors = require("cors");
 
 /*
  * Middlewares
  */
 
-app.use(cors({
-  credentials: true,
-  origin: [process.env.FRONTEND_URL]}))
-
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.FRONTEND_URL],
+  })
+);
 
 app.use(logger("dev")); // This logs HTTP reponses in the console.
 app.use(express.json()); // Access data sent as json @req.body
 app.use(express.urlencoded({ extended: false })); // Access data sent as urlEncoded (standard form or postman) @req.body
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
+// app.use(express.static(path.join(__dirname, "public"))); //This is for developing
+app.use(express.static(path.join(__dirname, "public/build"))); //This is for deloying
 
 app.use(
   session({
@@ -41,14 +42,51 @@ app.use(
  * Routes
  */
 
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(path.join(__dirname, "public/build/index.html"));
+  });
+}
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
-const itemRouter = require("./routes/item")
+const itemRouter = require("./routes/item");
 
 app.use("/", indexRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/items", itemRouter)
+app.use("/api/items", itemRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("*", (req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+  });
+}
+
+// 404 Middleware
+app.use((req, res, next) => {
+  const error = new Error("Ressource not found.");
+  error.status = 404;
+  next(error);
+});
+
+// Error handler middleware
+// If you pass an argument to your next function in any of your routes or middlewares
+// You will end up in this middleware
+// next("toto") makes you end up here
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV !== "production") {
+    console.error(err);
+  }
+  console.log(err);
+  console.log("An error occured");
+  res.status(err.status || 500);
+  if (!res.headersSent) {
+    res.json(err);
+  }
+});
 
 module.exports = app;
---
+++ server/public/build/precache-manifest.ba9f5dd8f375d40fc8405348f8db74bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e319e6caddb94ba843dc8f68c92176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2fa7e417c952e0abdd8",
+    "url": "/static/css/2.6ba6f2b8.chunk.css"
+  },
+  {
+    "revision": "2791811adb4e25c4b1f1",
+    "url": "/static/css/main.45faea09.chunk.css"
+  },
+  {
+    "revision": "c2fa7e417c952e0abdd8",
+    "url": "/static/js/2.3cf71888.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.3cf71888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2791811adb4e25c4b1f1",
+    "url": "/static/js/main.a424fe37.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
